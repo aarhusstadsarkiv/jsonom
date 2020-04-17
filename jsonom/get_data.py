cf0584eac07a045a94c0062debe0ca19dba83f40
Nina Jensen <jnik@aarhus.dk>
2020-04-17 12:37:42 +0200

    Update PronomData with get_lastest_file method

--- jsonom/get_data.py
+++ jsonom/get_data.py
@@ -3,12 +3,13 @@
 # -----------------------------------------------------------------------------
 
 import re
-from typing import List, Pattern, Literal, Optional
-import xmltodict
-import requests
-from requests import Response
-from bs4 import BeautifulSoup
+from typing import Any, Dict, Literal, Pattern
 
+import requests
+from bs4 import BeautifulSoup
+from requests import Response
+
+import xmltodict
 
 # -----------------------------------------------------------------------------
 # Classes
@@ -23,34 +24,26 @@ class PronomData:
     def __init__(self, url: str):
         self.url = url.strip("/")
 
-    def text(self, url: Optional[str] = None) -> str:
-        _url: str = url or self.url
-        _url = _url.strip("/")
-        response_data: Response = requests.get(f"{self.base_url}/{_url}")
+    def text(self) -> str:
+        response_data: Response = requests.get(f"{self.base_url}/{self.url}")
         return response_data.text
 
     def soup(self) -> BeautifulSoup:
         return BeautifulSoup(self.text(), features="html.parser")
 
-    def latest_file(self, file_type: Literal["signature", "container"]) -> str:
+    def latest_file(
+        self, file_type: Literal["signature", "container"]
+    ) -> Dict[Any, Any]:
         href_match: Pattern
         if file_type == "signature":
             href_match = re.compile(r"(?i)signature\s?file")
         elif file_type == "container":
             href_match = re.compile(r"(?i)container")
+        else:
+            raise ValueError(
+                "file_type must be either signature or container."
+            )
         link: str = self.soup().find_all(href=href_match)[-1].get("href")
-        return self.text(link)
-
-
-# -----------------------------------------------------------------------------
-# Function Definitions
-# -----------------------------------------------------------------------------
-
-
-# def get_latest_file(href_match: str) -> str:
-#     data = PronomData("aboutapps/pronom/droid-signature-files.htm")
-#     links: List[str] = []
-#     href_regex: Pattern = re.compile(href_match)
-#     for link in data.get_soup().find_all(href=href_regex):
-#         links.append(link.get("href"))
-#     return PronomData(links[-1]).get_text()
+        link_data: str = requests.get(f"{self.base_url}/{link}").text
+        latest_file: Dict[Any, Any] = xmltodict.parse(link_data)
+        return latest_file
