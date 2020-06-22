5624952601918468278725127b247f8ebfc498db
Nina Jensen <jnik@aarhus.dk>
2020-06-22 16:22:42 +0200

    Add different fixes for keys in Pronom Data

--- jsonom/parse_data.py
+++ jsonom/parse_data.py
@@ -2,38 +2,51 @@
 # Imports
 # -----------------------------------------------------------------------------
 
-import re
 import json
 from pathlib import Path
 from jsonom.pronom_data import PronomData
-from typing import Dict, Any
+from typing import Dict, Any, Callable, Union
 
 # -----------------------------------------------------------------------------
 # Auxiliary functions
 # -----------------------------------------------------------------------------
 
 
-def recurse_fix(in_dict: Dict[str, Any]) -> Dict[str, Any]:
-    new_dict = dict()
-    for key, value in in_dict.items():
-        if isinstance(value, dict):
-            return recurse_fix(value)
-        if isinstance(value, list):
-            for item in value:
-                recurse_fix(item)
-        new_key = key.strip("#@")
-        new_dict[new_key] = value
-    print(new_dict)
-    return new_dict
+def recurse_dict(func: Callable, value: Union[dict, list]) -> None:
+    if isinstance(value, dict):
+        func(value)
+    if isinstance(value, list):
+        for item in value:
+            if isinstance(item, dict):
+                func(item)
 
 
 def fix_keys(in_dict: Dict[str, Any]) -> Dict[str, Any]:
     keys = list(in_dict.keys())
     for key in keys:
-        if re.match(r"@|#", key):
-            print("Found key")
-            new_key = key.strip("@#")
-            in_dict[new_key] = in_dict.pop(key)
+        value = in_dict[key]
+        recurse_dict(fix_keys, value)
+        new_key = key.strip("#@")
+        in_dict[new_key] = in_dict.pop(key)
+    return in_dict
+
+
+def fix_byte_sequence(in_dict: Dict[str, Any]) -> Dict[str, Any]:
+    keys = list(in_dict.keys())
+    for key in keys:
+        value = in_dict[key]
+        recurse_dict(fix_byte_sequence, value)
+        if "ByteSequence" in key and isinstance(value, dict):
+            in_dict[key] = [value]
+    return in_dict
+
+
+def keys_to_lower(in_dict: Dict[str, Any]) -> Dict[str, Any]:
+    keys = list(in_dict.keys())
+    for key in keys:
+        value = in_dict[key]
+        recurse_dict(keys_to_lower, value)
+        in_dict[key.lower()] = in_dict.pop(key)
     return in_dict
 
 
@@ -48,7 +61,12 @@ def json_dump(data: dict, file: Path) -> None:
 # -----------------------------------------------------------------------------
 
 pronom_data = PronomData()
-signature_file = recurse_fix(pronom_data.latest_file("signature"))
+signature_file = pronom_data.latest_file("signature")
+json_dump(signature_file, Path("D:\\data\\signature_file.json"))
+
 container_file = pronom_data.latest_file("container")
 
-json_dump(signature_file, Path("D:\\data\\signature_file.json"))
+fixed_signature_file = keys_to_lower(
+    fix_byte_sequence(fix_keys(signature_file))
+)
+json_dump(fixed_signature_file, Path("D:\\data\\fixed_signature_file.json"))
